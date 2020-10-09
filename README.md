f0457b1ffb9ec822d48b583adb59784fcc072a24
Antoine Villeret <antoine@makula.be>
2020-10-09 11:47:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+rapidfuzz-test
+==============
+
+This is a simple project to test the rapidfuzz library CMake integration.
+
+# Building & run test program
+
+    git submodule update --init
+    mkdir build
+    cd build
+    cmake .. -GNinja 
+    ninja
+    ./foo
+
+
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <fuzz.hpp>
+
+int main(int,char**)
+{
+  std::string test = "aaaa";
+  std::string no_suffix = "aaa";
+  std::string no_suffix2 = "aaab";
+  std::string swapped1 = "abaa";
+  std::string swapped2 = "baaa";
+  std::string replace_all = "bbbb";
+
+  rapidfuzz::levenshtein::weighted_distance(test, test);
+  rapidfuzz::levenshtein::normalized_weighted_distance(test, test);
+}
